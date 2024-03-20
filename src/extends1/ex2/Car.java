b72c5f815f6598210db37258bbb4692af764a922
BMDK9 <qnrakdlzm77@naver.com>
2024-03-20 18:56:12 +0900

    extends

---
+++ src/extends1/ex2/Car.java
@@ -0,0 +1,7 @@
+package extends1.ex2;
+
+public class Car {
+    public void move() {
+        System.out.println("ex2 차를 이동합니다.");
+    }
+}
---
+++ src/extends1/overriding/Car.java
@@ -0,0 +1,10 @@
+package extends1.overriding;
+
+public class Car {
+    public void move() {
+        System.out.println("ex2 차를 이동합니다.");
+    }
+    public void openDoor() {
+        System.out.println("ex3 문을 연다.");
+    }
+}
